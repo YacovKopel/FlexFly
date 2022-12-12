9c8e3a98daf4f3517f5cb797c848c88717f2e7d6
Sky K <skyj.kim7@gmail.com>
2022-12-12 15:17:15 -0800

    added clickable cards

--- assets/script.js
+++ assets/script.js
@@ -1,114 +1,127 @@
-var submitBtn= $('#submit');
-var from=$('#fromInput');
-var to = $('#toInput');
-var duration = $('#durationInput');
-var numMonths = $('#numMonths');
-var one= $('#plusOne');
-var two =$('#plusTwo');
-var three= $('#plusThree');
-var four=$('#plusFour');
-var five = $('#plusFive')
-var six = $('#plusSix');
-var today=dayjs().format('DD/MM/YYYY');
-$('#time').text(today);
+var submitBtn = $("#submit");
+var from = $("#fromInput");
+var to = $("#toInput");
+var duration = $("#durationInput");
+var numMonths = $("#numMonths");
+var one = $("#plusOne");
+var two = $("#plusTwo");
+var three = $("#plusThree");
+var four = $("#plusFour");
+var five = $("#plusFive");
+var six = $("#plusSix");
+var today = dayjs().format("DD/MM/YYYY");
+var cardOne = $(".card-1");
+var cardTwo = $(".card-2");
+var cardThree = $(".card-3");
+$("#time").text(today);
 
-var uptodate=''
+var uptodate = "";
 function returnMon() {
-    if (numMonths.val()=='Now - 1 month') {
-        uptodate= dayjs().add(1, 'month').format('DD/MM/YYYY') 
-    }
-    else if (numMonths.val()=='Now - 2 months') {
-        uptodate= dayjs().add(2, 'month').format('DD/MM/YYYY') 
-    }else if (numMonths.val()=='Now - 3 months') {
-        uptodate= dayjs().add(3, 'month').format('DD/MM/YYYY') 
-    }
-    else if (numMonths.val()=='Now - 4 months') {
-        uptodate= dayjs().add(4, 'month').format('DD/MM/YYYY') 
-    }
-    else if (numMonths.val()=='Now - 5 months') {
-        uptodate= dayjs().add(5, 'month').format('DD/MM/YYYY') 
-    }else if (numMonths.val()=='Now - 6 months') {
-        uptodate= dayjs().add(6, 'month').format('DD/MM/YYYY') 
-    }
-    return uptodate
+  if (numMonths.val() == "Now - 1 month") {
+    uptodate = dayjs().add(1, "month").format("DD/MM/YYYY");
+  } else if (numMonths.val() == "Now - 2 months") {
+    uptodate = dayjs().add(2, "month").format("DD/MM/YYYY");
+  } else if (numMonths.val() == "Now - 3 months") {
+    uptodate = dayjs().add(3, "month").format("DD/MM/YYYY");
+  } else if (numMonths.val() == "Now - 4 months") {
+    uptodate = dayjs().add(4, "month").format("DD/MM/YYYY");
+  } else if (numMonths.val() == "Now - 5 months") {
+    uptodate = dayjs().add(5, "month").format("DD/MM/YYYY");
+  } else if (numMonths.val() == "Now - 6 months") {
+    uptodate = dayjs().add(6, "month").format("DD/MM/YYYY");
+  }
+  return uptodate;
 }
 
-var url = 'https://api.tequila.kiwi.com/v2/search';
-var apikey = '9dU5c1zZxOO4AyOA58aEW70owtRgoHgC';
+var url = "https://api.tequila.kiwi.com/v2/search";
+var apikey = "9dU5c1zZxOO4AyOA58aEW70owtRgoHgC";
 
-var fromCity='';
+var fromCity = "";
 function getFlightUrl(fromCity) {
-        
-    var newurl= 'https://api.tequila.kiwi.com/v2/search' + '?fly_from='+ fromCity
-     + '&fly_to=' + to.val()+'&return_from='+ today +'&return_to='+ uptodate + '&nights_in_dst_from=' + duration.val() 
-     + '&nights_in_dst_to=' + duration.val()+'&flight_type=round' + '&curr=USD&max_stopovers=1&sort=price&asc=1&limit=3'      
-    console.log(newurl)
-    fetch(newurl, {
-        method: "GET",
-        withCredentials: true,
-        headers: {
-          "apikey": apikey,
-          "Content-Type": "application/json"
+  var newurl =
+    "https://api.tequila.kiwi.com/v2/search" +
+    "?fly_from=" +
+    fromCity +
+    "&fly_to=" +
+    to.val() +
+    "&return_from=" +
+    today +
+    "&return_to=" +
+    uptodate +
+    "&nights_in_dst_from=" +
+    duration.val() +
+    "&nights_in_dst_to=" +
+    duration.val() +
+    "&flight_type=round" +
+    "&curr=USD&max_stopovers=1&sort=price&asc=1&limit=3";
+  console.log(newurl);
+  fetch(newurl, {
+    method: "GET",
+    withCredentials: true,
+    headers: {
+      apikey: apikey,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then(function (data) {
+      console.log(data);
+      console.log(data.data.length);
+      for (let i = 0; i < data.data.length; i++) {
+        var fullflight = data.data[i].route;
+        console.log(data.data[i].price);
+        console.log(fullflight);
+        for (let i = 0; i < fullflight.length; i++) {
+          console.log(fullflight.length);
+          console.log(fullflight[i].cityCodeFrom);
+          console.log(fullflight[i].cityCodeTo);
+          console.log(fullflight[i].airline);
+          console.log(fullflight[i].cityFrom);
+          console.log(fullflight[i].cityTo);
+          console.log(fullflight[i].flight_no);
         }
-      })
-        .then(resp => resp.json())
-        .then(function(data) {
-          console.log(data);
-          console.log(data.data.length)
-          for (let i = 0; i < data.data.length; i++) {
-            var fullflight=data.data[i].route
-            console.log(data.data[i].price)
-            console.log(fullflight)
-            for (let i = 0; i < fullflight.length; i++) {
-                console.log(fullflight.length)
-                console.log(fullflight[i].cityCodeFrom)
-                console.log(fullflight[i].cityCodeTo)
-                console.log(fullflight[i].airline)
-                console.log(fullflight[i].cityFrom)
-                console.log(fullflight[i].cityTo)
-                console.log(fullflight[i].flight_no)
-        
-            }
-        }
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 async function getCityCode() {
-    var citynewurl= 'https://api.tequila.kiwi.com/locations/query' + '?term='+ from.val()+'&locale=en-US&location_types=city&limit=10&active_only=true'      
-    
-    console.log(citynewurl)
-    await fetch(citynewurl, {
-        method: "GET",
-        withCredentials: true,
-        headers: {
-          "apikey": apikey,
-          "Content-Type": "application/json"
-        }
-      })
-        .then(resp => resp.json())
-        .then(function(data){
-           fromCity= data.locations[0].code
-           console.log(fromCity)
-           getFlightUrl(fromCity)
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-       
+  var citynewurl =
+    "https://api.tequila.kiwi.com/locations/query" +
+    "?term=" +
+    from.val() +
+    "&locale=en-US&location_types=city&limit=10&active_only=true";
+
+  console.log(citynewurl);
+  await fetch(citynewurl, {
+    method: "GET",
+    withCredentials: true,
+    headers: {
+      apikey: apikey,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => resp.json())
+    .then(function (data) {
+      fromCity = data.locations[0].code;
+      console.log(fromCity);
+      getFlightUrl(fromCity);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
-$(document).ready(function(){
-    submitBtn.on('click',function(event){
-        event.preventDefault();
-        returnMon(uptodate);
-        getCityCode()
-        
-         // Submit the form
-    });
-});
+$(document).ready(function () {
+  submitBtn.on("click", function (event) {
+    event.preventDefault();
+    returnMon(uptodate);
+    getCityCode();
 
+    // Submit the form
+  });
+});
 
 // function autocomplete(inp, arr) {
 //     /*the autocomplete function takes two arguments,
@@ -207,7 +220,6 @@ $(document).ready(function(){
 //   });
 //   };
 //   autocomplete(document.getElementById("fromInput"), countries);
-
 
 // need from input to switch to code from location api
 // then do same for return city
